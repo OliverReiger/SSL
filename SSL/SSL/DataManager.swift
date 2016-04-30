456dcfb2db09497d5507d0120513e0856a2de6f3
Andreas Seitz <seitz@in.tum.de>
2016-04-30 15:37:16 +0200

    DataManager Setup and ready to go

---
+++ SSL/SSL/DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  SSL
+//
+//  Created by Andreas Seitz on 30/04/16.
+//  Copyright © 2016 Oliver Reiger. All rights reserved.
+//
+
+import Cocoa
+
+class DataManager: NSObject {
+
+}
---
+++ SSL/SSL/DummyDataGenerator.swift
@@ -0,0 +1,9 @@
+//
+//  DummyDataGenerator.swift
+//  SSL
+//
+//  Created by Andreas Seitz on 30/04/16.
+//  Copyright © 2016 Oliver Reiger. All rights reserved.
+//
+
+import Foundation
